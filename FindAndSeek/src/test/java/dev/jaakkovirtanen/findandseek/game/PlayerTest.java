c3986c04e053cb8c2592ef3186b25f70c4ddc5c0
Jaakko Virtanen <jaakvirt@dhcp-asv-94.helsinki.fi>
2016-12-28 16:42:35 +0200

    first tests for Player class

---
+++ FindAndSeek/src/test/java/dev/jaakkovirtanen/findandseek/game/PlayerTest.java
@@ -0,0 +1,59 @@
+package dev.jaakkovirtanen.findandseek.game;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    private Player player;
+
+    public PlayerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        this.player = new Player();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void playerConstructor() {
+        assertEquals(1, this.player.getCol());
+        assertEquals(1, this.player.getRow());
+        MoveBehaviour expected = new MoveCardinal();
+        assertEquals(expected.getClass(), this.player.moveBehaviour.getClass());
+    }
+
+    @Test
+    public void getCol() {
+        assertEquals(1, this.player.getCol());
+    }
+
+    @Test
+    public void getRow() {
+        assertEquals(1, this.player.getRow());
+    }
+
+    @Test
+    public void changeMoveBehaviour() {
+        MoveBehaviour expected = new MoveDiagonally();
+        this.player.changeMoveBehaviour(expected);
+        assertEquals(expected.getClass(), player.moveBehaviour.getClass());
+    }
+
+}
