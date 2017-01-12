29bcf59f6cb2ddce829cb8e9546e37ed84b2ad7b
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-01-12 06:38:16 -0500

    c

--- FindAndSeek/src/main/java/dev/jaakkovirtanen/findandseek/game/Main.java
+++ FindAndSeek/src/main/java/dev/jaakkovirtanen/findandseek/game/Main.java
@@ -12,9 +12,6 @@ public class Main {
      */
     public static void main(String[] args) throws InterruptedException {
         GUI gui = new GUI();
-        PopUpWindow wndw = new PopUpWindow();
-        wndw.messageWindow("gfjasodgjoi");
-        wndw.congratsWindow(10);
         gui.run();
     }
 }
